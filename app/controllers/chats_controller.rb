f00e54e2c3a4b7b5d19dc682402890588cec281a
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-14 21:30:37 +0100

    - add controller actions

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,20 @@
 class ChatsController < ApplicationController
-  def create
+  def index
+    @chats = Chat.all
+    @chat = Chat.new
   end
 
-  def index
+  def create
+    @chat = Chat.new(chat_params)
+
+    respond_to do |format|
+      if @chat.save
+        format.html { redirect_to @chat, notice: 'Message was successfully posted.' }
+        format.json { render :show, status: :created, location: @chat }
+      else
+        format.html { render :new }
+        format.json { render json: @chat.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
