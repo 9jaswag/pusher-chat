fe8319434546eaf28e309e1740dd8c202ecf2e91
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-14 23:03:12 +0100

    - add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :chats, only: %i(index create)
+
+  resources :chats
   root 'chats#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
