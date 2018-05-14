8bfdae1ae3108d825dc77ba09e6b3a6d7ab12c14
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-14 21:40:02 +0100

    - update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'chats/create'
-
-  get 'chats/index'
+  resources :chats, only: %i(index create)
+  root 'chats#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
