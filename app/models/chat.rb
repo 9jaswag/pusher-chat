d3d1753e9498fbdedc82e9bfc700475b43e760be
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-16 12:49:27 +0100

    - some code cleanup

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ApplicationRecord
-  after_commit :notify_pusher, on: [:create, :update]
+  after_create :notify_pusher, on: :create
 
   def notify_pusher
     Pusher.trigger('chat', 'new', self.as_json)
